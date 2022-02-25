edd3b1848b1d228f30e759661b1be9659f2654c6
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2022-02-25 12:00:52 +0100

    Removed obsolete variables, added docstring/commentary

--- Inleveropgave 3/main.cpp
+++ Inleveropgave 3/main.cpp
@@ -4,15 +4,20 @@
 using std::cout, std::endl, std::to_string, std::vector, std::reverse, std::copy;
 
 vector<int> merge_sort(vector<int> xs) {
+    /*
+    In-place merge sort of vector without recursion. The basic idea
+    is to avoid the recursive call while using an iterative solution.
+    The algorithm first merges a chunk of a length of 2, then it merges 
+    chunks the length of 4, then 8, 16, .... , until 2^k where 2^k is larger
+    than the length of the array. It then returns the now sorted list.
+    */
     int unit = 1;
     while (unit <= xs.size()) {
-        int h = 0;
         for (int h = 0; h < xs.size(); h += (unit*2)) {
-            int l = h;
             vector<int> v = {int(xs.size()), h + 2 * unit};
             int r = *min_element(v.begin(), v.end());
             int mid = h + unit;
-            int p = l;
+            int p = h;
             int q = mid;
 
             while (p < mid && q < r) {
@@ -35,7 +40,7 @@ vector<int> merge_sort(vector<int> xs) {
 
 int main() {
 
-    vector<int> input = {123, 53, 2, 6, 75, 35, 78, 29, 10, 90};
+    vector<int> input = {123, -53, 2, 6, 75, 35, 7800, -29, 10, 90};
     vector<int> result = merge_sort(input);
 
     for (int i : result) {
