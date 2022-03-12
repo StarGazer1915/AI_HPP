c85500b8bca98d6cdc7fe79c004bccaaf85b8cf8
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2022-03-12 15:36:20 +0100

    Working on Multi-threaded merge sort now, problems with getting return vectors.

--- Inleveropgave 3b/main.cpp
+++ Inleveropgave 3b/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <future>
 
 using std::cout, std::endl, std::to_string, std::vector, std::thread;
 
@@ -86,34 +87,28 @@ vector<int> multi_thread_merge_sort(vector<int> xs, int num_of_threads) {
     thread thr[num_of_threads];
     vector<vector<int>> sliced_vector;
 
-    int slice = 0;
-    for (int i = 0 ; i < num_of_threads ; i++) {
-        vector<int> v_part(xs.begin() + slice, xs.begin() + slice + part);
+    for (int slice = 0 ; slice < num_of_threads ; slice += part) {
+        vector<int> v_part(xs.begin() + slice, xs.begin() + slice + part - 1);
         sliced_vector.push_back(v_part);
-        slice += part;
     }
 
-    for (int i = 0 ; sliced_vector.size() == 1 ; i++) {
-        thr[t] = thread(sort_and_merge, sliced_vector[i], sliced_vector[i+1])
-
-
+    int t = 0;
+    while (sliced_vector.size() != 1) {
+        for (int i = 0 ; i < sliced_vector.size() ; i += 2) {
+            thr[t] = thread(sort_and_merge, sliced_vector[i], sliced_vector[i+1]);
+        }
+        for (int t = 0; t < num_of_threads; t++) {
+            thr[t].join();
+        }      
     }
-    for (int i = 0 ; i < sliced_vector.size() ; i += 2) {
-        thr[t] = thread(sort_and_merge, sliced_vector[i], sliced_vector[i+1])
-    }
-
-
-    for (t = 0; t < num_of_threads; t++)
-    {
-        thr[t].join();
-    }
+    return sliced_vector[0];    
 }
 
 
 int main() {
 
     vector<int> input = {12, 902, -3, 85, 93, -24, 7, 432};
-    vector<int> result = merge_sorted_arrays(input1, input2);
+    vector<int> result = multi_thread_merge_sort(input, 4);
 
     for (int i : result) {
         cout << to_string(i) << endl;
