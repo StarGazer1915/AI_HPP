cf1bf676e0f8e5e269815f4efc3849f1c9e1aa93
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2022-03-19 20:13:06 +0100

    Added master construct, changed code structure, removed redundant code

--- Inleveropgave 4/src/ompArraySum.cpp
+++ Inleveropgave 4/src/ompArraySum.cpp
@@ -4,10 +4,8 @@
 #include <iostream>
 #include <fstream>
 #include <omp.h> // OpenMP
-#include <thread>
-#include <vector>
 
-using std::cout, std::endl, std::thread, std::vector;
+using std::cout, std::endl;
 
 void readArray(char *fileName, double **a, int *n);
 double sumArray(double *a, int numValues);
@@ -17,8 +15,7 @@ int main(int argc, char *argv[])
     int howMany;
     double sum = 0;
     double *a;
-    int thread_sum[4];
-    omp_set_num_threads(4);
+    omp_set_num_threads(2);
     double start = omp_get_wtime();
 
     if (argc != 2)
@@ -27,53 +24,24 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // SERIAL IMPLEMENTATION
-    readArray(argv[1], &a, &howMany);
-    sum = sumArray(a, howMany);
-
-    printf("\nSerial Version: %f", sum);
-
     // PARALLEL IMPLEMENTATION
-    sum = 0;
-    readArray(argv[1], &a, &howMany);
-    #pragma omp parallel reduction (+:sum)
+    #pragma omp master
     {
-        int ID = omp_get_thread_num();
-        thread_sum[ID] = 0;
-        
-        #pragma omp for
-        for (int i = 0; i < 10000; i++) {
-            thread_sum[ID] += a[i];
+        readArray(argv[1], &a, &howMany);
+        #pragma omp parallel for reduction (+:sum)
+        for (int i = 0; i < howMany; i++) {
+            sum += a[i];
         }
+        cout << "\nThe sum of the values in the input file '" << argv[1] << "' is " << sum << endl;
+    
+        double end = omp_get_wtime();
+        cout << "Elasped time = " << (end - start) << " sec\n" << endl;
     }
-    // for (int i = 0; i < 4; i++) {
-    //     sum += thread_sum[i];
-    // }
-    
-    printf("\nParallel Version: %f", sum);
-    // cout << "The sum of the values in the input file '" << argv[1] << "' is " << sum << endl;
-    
-    double end = omp_get_wtime();
-    cout << "\nElasped time = " << (end - start) << " sec" << endl;
     
     free(a);
     return 0;
 }
 
-vector<vector<int>> slice_vector(vector<int> xs, int num_of_threads) {
-    /*
-    Divides a vector into slices/parts for each thread.
-    So the vector will be split into four subvectors should the num_of_threads be 4.
-    */
-    int part = xs.size() / num_of_threads;
-    vector<vector<int>> sliced_vector;
-
-    for (int slice = 0 ; slice < xs.size() ; slice += part) {
-        vector<int> v_part(xs.begin() + slice, xs.begin() + slice + part);
-        sliced_vector.push_back(v_part);
-    }
-    return sliced_vector;
-}
 
 /* readArray fills an array with values from a file.
  * Receive: fileName, a char*,
