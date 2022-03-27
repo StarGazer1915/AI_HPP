ccfa5d4f7affb988971b5513e31e2fbbb2a09f5c
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2022-03-27 21:29:23 +0200

    Added extra comments and moved sqrt max_n variable declaration

--- Eindopdracht/src/mpi-openmp.cpp
+++ Eindopdracht/src/mpi-openmp.cpp
@@ -10,7 +10,7 @@ using std::cout, std::endl, std::cin, std::to_string ,std::vector;
 
 int main(int argc, char *argv[]) 
 {   
-   unsigned long long n = 1000000;
+   unsigned long long n = 1000000000;
    vector<bool> table(n, true);
    int rank, comm_size;
    omp_set_num_threads(4);
@@ -18,13 +18,15 @@ int main(int argc, char *argv[])
    double startTime = 0.0, totalTime = 0.0;
    startTime = MPI_Wtime();
 
+   unsigned long long max_n = sqrt(n);
+
    MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_size);   
 
-   unsigned long long max_n = sqrt(n);
    #pragma omp parallel for
    for (unsigned long long p = 2; p < max_n; p++) {
+      if (p%comm_size != rank) continue;
       if (table[p] == true) {
          for (unsigned long long i = p * p; i < n; i += p)
             table[i] = false;
@@ -37,6 +39,7 @@ int main(int argc, char *argv[])
    cout << "\nProcess " << rank << " finished in " << totalTime << " seconds." << endl;
    
    // ===== Uncomment the code below to print all values in terminal =====
+   // ===== NOTE: This might print confusing results if you're working with multiple MPI processes. =====
    // for (unsigned long long p = 2; p < n; p++) {
    //    if (table[p])
    //       cout << p << " ";
